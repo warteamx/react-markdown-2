982ee79e3dc5371e2147b2eacf5c89fe75bfd365
martinrebo <martinrg33@gmail.com>
2019-02-16 09:07:05 +0100

    add editor to index

--- src/editor/editor.js
+++ src/editor/editor.js
@@ -2,11 +2,22 @@ import React, { Component } from 'react';
 import './editor.css';
 
 class Editor extends Component {
+constructor(props) {
+  super(props);
+  this.state ={value: ''};
+  this.handleChange = this.handleChange.bind(this)
+}
+
+handleChange(event) {
+  this.setState({value: event.target.value})
+}
+
   render() {
     return (
-      <textarea id="editor">
-this is some text area
+      <textarea id="editor" value={this.state.value} onChange={this.handleChange}>
+
       </textarea>
+      
     );
   }
 }
